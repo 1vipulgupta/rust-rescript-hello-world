74597a55eb156e421b83cce8b59c169aa1cc8ed7
vipul.gupta <vipulgupta42all@gmail.com>
2023-06-14 21:03:10 +0530

    temp

---
+++ builder.sh
@@ -0,0 +1,3 @@
+mkdir $out
+cd $out
+rustc $src/hello.rs
--- flake.nix
+++ flake.nix
@@ -14,16 +14,25 @@
     {
         foo = "bar";
         packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-        packages.x86_64-linux.default = self.packages.x86_64-linux.hello; #need to point it to rust, so can build rust by default.
-        # devShells.default = nixpkgs.mkShell {
-        #     packages = [ pkgs.rustc pkgs.coreutils ];
+        packages.x86_64-linux.mkderivatio = pkgs.stdenv.mkDerivation {
+                                                                    name = "rust-flake";
+                                                                    builder = "${pkgs.bash}/bin/bash";
+                                                                    args = [ ./builder.sh ];
+                                                                    inherit (pkgs) rustc coreutils cowsay hello;
+                                                                    src = ./.;
+                                                                    system = builtins.currentSystem;
+                                                                    PATH = "${pkgs.coreutils}/bin:${pkgs.rustc}/bin:${pkgs.gcc}/bin";
+                                                                };
+        packages.x86_64-linux.default = self.packages.x86_64-linux.mkderivatio; #need to point it to rust, so can build rust by default.
+        devShells.default = nixpkgs.mkShell {
+            packages = [ pkgs.rustc pkgs.coreutils ];
 
-        #     inputsFrom = [ ];
+            inputsFrom = [ ];
 
-        #     shellHook = ''
-        #         export DEBUG=1
-        #         rustc hello.rs
-        #     '';
-        # };
+            shellHook = ''
+                export DEBUG=1
+                rustc hello.rs
+            '';
+        };
     };
 }
