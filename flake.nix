44a5e9ebe7cc428ec2e3b84ddde8fd3896ee2e92
vipul.gupta <vipulgupta42all@gmail.com>
2023-06-17 14:08:29 +0530

    Added git ignore and clean up

--- flake.nix
+++ flake.nix
@@ -7,31 +7,30 @@
         flake-parts = { url = "github:hercules-ci/flake-parts"; inputs.nixpkgs-lib.follows = "nixpkgs"; };
         flake-utils = { url = "github:numtide/flake-utils"; };
     };
-    outputs = {self, nixpkgs, ...}: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-    {
-        foo = "bar";
-        packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-        packages.x86_64-linux.mkderivatio = pkgs.stdenv.mkDerivation {
-                                                                    name = "main";
-                                                                    builder = "${pkgs.bash}/bin/bash";
-                                                                    args = [ ./builder.sh ];
-                                                                    inherit (pkgs) rustc coreutils cowsay hello;
-                                                                    src = ./.;
-                                                                    system = builtins.currentSystem;
-                                                                    PATH = "${pkgs.coreutils}/bin:${pkgs.rustc}/bin:${pkgs.gcc}/bin";
-                                                                };
-        packages.x86_64-linux.default = self.packages.x86_64-linux.mkderivatio; #need to point it to rust, so can build rust by default.
-        # devShells.default = nixpkgs.mkShell {
-        #     packages = [ pkgs.rustc pkgs.coreutils ];
-
-        #     inputsFrom = [ ];
-
-        #     shellHook = ''
-        #         export DEBUG=1
-        #         rustc hello.rs
-        #     '';
-        # };
-    };
+    outputs = {self, nixpkgs, flake-utils, ...}:
+        flake-utils.lib.eachDefaultSystem (system: let
+            pkgs = nixpkgs.legacyPackages.${system};
+            in
+            {   # nix develop
+                # defaultPackage = pkgs.mkShell { #create a new shell with hook > compiles main.rs as run ./main
+                #                                 buildInputs = [ pkgs.cargo pkgs.rustc ];
+                #                                 inputsFrom = [ ];
+                #                                 shellHook = ''
+                #                                     export DEBUG=1
+                #                                     rustc main.rs
+                #                                     ./main
+                #                                     rm main
+                #                                 '';
+                #                               };
+                packages.default = pkgs.stdenv.mkDerivation #nix run
+                    {
+                        name = "main";
+                        builder = "${pkgs.bash}/bin/bash";
+                        args = [ ./builder.sh ];
+                        inherit (pkgs) rustc coreutils cargo;
+                        src = ./.;
+                        system = builtins.currentSystem;
+                        PATH = "${pkgs.coreutils}/bin:${pkgs.rustc}/bin:${pkgs.gcc}/bin";
+                    };
+            });
 }
